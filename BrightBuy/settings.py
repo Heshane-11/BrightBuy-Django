662604f2ed553216dfe20d19f2940965d05a4d32
Heshane-11 <hishu3851@gmail.com>
2026-01-02 23:08:43 +0530

    fixing evrything1

--- BrightBuy/settings.py
+++ BrightBuy/settings.py
@@ -29,6 +29,14 @@ CSRF_TRUSTED_ORIGINS = config(
     cast=lambda v: [s.strip() for s in v.split(',')]
 )
 
+import cloudinary
+
+cloudinary.config(
+    cloud_name=config('CLOUDINARY_CLOUD_NAME'),
+    api_key=config('CLOUDINARY_API_KEY'),
+    api_secret=config('CLOUDINARY_API_SECRET'),
+    secure=True
+)
 
 
 
@@ -176,6 +184,7 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': config('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': config('CLOUDINARY_API_KEY'),
     'API_SECRET': config('CLOUDINARY_API_SECRET'),
+    'SECURE': True
 }
 
 
